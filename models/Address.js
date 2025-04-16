bf14e1006e5acb9754eebab8368dc1ba1ba36f15
Yigitcanozsahin <135243284+Yigitcanozsahin@users.noreply.github.com>
2025-04-16 19:14:55 +0300

    ..
    .

--- models/Address.js
+++ models/Address.js
@@ -7,6 +7,7 @@ const AddressSchema = mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true },
+    phone: { type: String, required: true },
     address: { type: String, required: true },
     city: { type: String, required: true },
     district: { type: String },
